0b1b1f9805ef0b67bc1e04ada3125ba0c6b461f6
Peter Law <PeterJCLaw@gmail.com>
2016-03-25 21:27:58 +0000

    Implement direction-to-top validation
    This appears to mostly work, based on the (correct) token I have to
    hand. There appear to be some cases where three markers are in sight
    and a (false) error is found. In such cases, the angle between one
    pair of markers is in the region 30-35 degrees, which is quite a
    large error.
    
    Change-Id: I5721edf8bb804a2f7bbd0f023c047d77173ab8de

--- comp16/token-check/check.py
+++ comp16/token-check/check.py
@@ -8,16 +8,35 @@ from sr.robot.vision import ( Vision, C500_focal_length,
                               MARKER_TOP, MARKER_BOTTOM, MARKER_SIDE,
                               NET_A, NET_B, NET_C )
 
-from marker_helpers import ( get_direction_out_from_face,
+from marker_helpers import ( get_direction_behind_face,
+                             get_direction_out_from_face,
                              get_direction_to_right,
                              get_direction_to_top,
                              get_net )
 
 from term import print_fail, print_ok, print_warn
-from vectors import make_vector, are_same_direction
+from vectors import make_vector, angle_between, are_same_direction
 
+# Be very generous with the tolerance
+import vectors
+vectors.DEGREES_TOLERANCE = 30
 
 RES = (1280,1024)
+
+
+def get_direction_to_token_top(marker):
+    kind = marker.info.marker_type
+    if kind == MARKER_SIDE:
+        return get_direction_to_top(marker)
+
+    elif kind == MARKER_TOP:
+        return get_direction_out_from_face(marker)
+
+    elif kind == MARKER_BOTTOM:
+        return get_direction_behind_face(marker)
+
+    else:
+        assert False, "Unexpected marker type: {0}.".format(kind)
 
 
 # Via the itertools docs: https://docs.python.org/2/library/itertools.html#recipes
@@ -48,13 +67,22 @@ def process(markers):
     else:
         print_ok(net)
 
-    if len(markers) > 1:
-        # do processing to check the validity
+    if len(markers) < 2:
+        print_warn("Only one marker in sight")
+        return
 
-        dirs_to_top = map(get_direction_to_top, markers)
-        all_same_direction = all(are_same_direction(*p) for p in pairwise(dirs_to_top))
-        print("all_same_direction:", all_same_direction)
+    # do processing to check the validity
 
+    dirs_to_top = map(get_direction_to_token_top, markers)
+    all_same_direction = all(are_same_direction(*p) for p in pairwise(dirs_to_top))
+    if all_same_direction:
+        print_ok("Token valid")
+    else:
+        print_fail("Token invalid -- directions disagree!")
+        for m in markers:
+            print_fail('-', m.info.marker_type, m.info.code)
+        for p in pairwise(dirs_to_top):
+            print(angle_between(*p))
 
 while True:
     markers = see()
